29bc5815c86220042c27997ac547e8c117ed7ac8
Murilo Beluco <murilobeluco@yahoo.com>
2020-01-06 12:54:48 -0300

    Alteração no programa de horario e foi adicionado o Shutup10

--- script.ps1
+++ script.ps1
@@ -3,82 +3,58 @@ Update-ExecutionPolicy Unrestricted
 Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowHiddenFilesFoldersDrives -DisableShowFrequentFoldersInQuickAccess -enableShowRibbon
 Set-TaskbarOptions -Combine Never
 Disable-BingSearch
-
-choco install Firefox googlechrome notepadplusplus.install spotify discord.install fastcopy battle.net twitch authy-desktop megasync dropbox 7zip.install sharex  jre8 javaruntime freedownloadmanager steam geforce-experience -y --ignore-checksums
-
+ 
+choco install Firefox googlechrome notepadplusplus.install shutup10 qbittorrent spotify discord.install fastcopy battle.net twitch authy-desktop megasync dropbox 7zip.install sharex  jre8 javaruntime freedownloadmanager steam razer-synapse-2 geforce-experience -y --ignore-checksums
+ 
 Write-BoxstarterMessage 'Desativando modo de hibernacao'
 powercfg -h off
-
+ 
 Write-BoxstarterMessage 'Mudando configuracao de energia: timeout do HD nunca'
 powercfg -change -disk-timeout-ac 0
 powercfg -change -disk-timeout-dc 0
-
+ 
 Write-BoxstarterMessage 'Mudando configuracao de energia: Desligar monitor apos 20 minutos'
-powercfg -change -monitor-timeout-ac 20
-powercfg -change -monitor-timeout-dc 20
-
+powercfg -change -monitor-timeout-ac 10
+powercfg -change -monitor-timeout-dc 10
+ 
 Write-BoxstarterMessage 'Mudando configuracao de energia: Standby do computador para nunca'
 powercfg -change -standby-timeout-ac 0
 powercfg -change -standby-timeout-dc 0
-
+ 
 Set-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name 'InitialKeyboardIndicators' -Value 2
-
-Write-BoxstarterMessage 'Configurando horario do windows'
+ 
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\7-Zip\", [EnvironmentVariableTarget]::Machine)
 refreshenv
-
+ 
 New-Item -ItemType directory -Path 'C:\layout\'
-
+ 
 Get-Item 'c:\layout' -Force | ForEach-Object { $_.Attributes = $_.Attributes -bor 'Hidden' }
-
-Invoke-WebRequest 'https://www.horstmuc.de/win64/timesync64.zip' -outFile 'c:\layout\timesync64.zip'
-
-& 7z.exe e c:\layout\timesync64.zip -oc:\layout\ *.exe -r -y
-
-Start-Sleep -Seconds 10
-
-Write-BoxstarterMessage 'atualizando horario'
-Set-Location C:\layout\
-
-Invoke-WebRequest 'https://pastebin.com/raw/WnXic8v9' -outFile 'c:\layout\timesync.ini'
-& c:\layout\timesync.exe /auto
-Start-Sleep -Seconds 20
-Stop-Process -Name timesync
-Remove-Item C:\layout\timesync64.zip
-
+ 
+ 
 Write-BoxstarterMessage 'Configurando ShareX'
-Invoke-WebRequest 'https://dl.dropbox.com/s/mm1gyg1uha3vwae/ShareX-12.3.1-backup.sxb' -outFile 'c:\layout\sharexconfig.zip'
-						    
+Invoke-WebRequest 'https://dl.dropbox.com/s/8ns8lh850n5ppuh/ShareX-13.0.1-backup.sxb' -outFile 'c:\layout\sharexconfig.zip'
+                           
 $caminho = [environment]::getfolderpath("mydocuments") +'\' + 'Sharex'
 $comando = '& 7z.exe e c:\layout\sharexconfig.zip -o'+$caminho+' -y'
 Stop-Process -Name sharex | out-null
 Invoke-Expression $comando | out-null
 
-Write-BoxStarterMessage 'Criando tarefa do horario'
-$User= $env:UserName
-$Trigger= New-ScheduledTaskTrigger -AtLogon
-$Action= New-ScheduledTaskAction -Execute 'C:\layout\timesync.exe' -Argument '/auto' -WorkingDirectory 'c:\layout\'
-Register-ScheduledTask -TaskName 'Atualizar Horario' -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
-
+ 
 function CriarRegistro {
     Param ([string]$a)
      
     if (-Not(Test-Path $a)) {
         New-Item -Path $a -Force | out-null
-    }     
+    }    
 }
-
-Write-BoxStarterMessage 'Razer Surround'
-Invoke-WebRequest 'https://d289qh4hsbjjw7.cloudfront.net/razerzone-20150303011856922/files/RazerSurroundInstaller2.0.29.20.exe' -outFile 'c:\layout\RazerSurroundInstaller2.0.29.20.exe'
-Start-Process 'c:\layout\RazerSurroundInstaller2.0.29.20.exe'
-
+ 
 function CarregarExplorer {
     Write-BoxStarterMessage 'Parando o processo Explorer.exe'
     Stop-Process -Name Explorer | out-null
     gpupdate /force
     Start-Sleep 10
 }
-
+ 
 Write-BoxStarterMessage 'Alterando configuracoes do registro'
 CriarRegistro('HKLM:\Software\Policies\Microsoft\Windows\Explorer')
 CriarRegistro('HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization')
@@ -88,17 +64,17 @@ CriarRegistro('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesk
 CriarRegistro('HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer')
 CriarRegistro('HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People')
 CriarRegistro('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel')
-
+ 
 #configurando menu iniciar
 Write-BoxStarterMessage 'Configurando menu iniciar'
 Invoke-WebRequest 'https://pastebin.com/raw/Rag0n8dQ' -outFile 'c:\layout\layout.xml'
 Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'LockedStartLayout' -Type DWord -Value 1
 Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'StartLayoutFile' -Type ExpandString -Value 'c:\layout\layout.xml'
-CarregarExplorer 
+CarregarExplorer
 Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'LockedStartLayout' -Type DWord -Value 0
 Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'StartLayoutFile' -Type ExpandString -Value ''
 CarregarExplorer
-
+ 
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization' -Name NoLockScreen -Type DWord -Value 1
 Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\Explorer' -Name 'HideRecentlyAddedApps' -Type DWord -Value 1
 Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSyncNGSC' -Type DWord -Value 1
@@ -113,7 +89,7 @@ Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel' -Name '{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -Type DWord -Value 0
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu' -Name '{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -Type DWord -Value 0
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarSmallIcons' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop' -Name 'IconSize' -Type DWord -Value 32
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop' -Name 'IconSize' -Type DWord -Value 30
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop' -Name 'Mode' -Type DWord -Value 1
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop' -Name 'LogicalViewMode' -Type DWord -Value 3
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Start_TrackDocs' -Type DWord -Value 0
@@ -123,3 +99,30 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Nam
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Free Download Manager'
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Spotify'
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Discord'
+ 
+Write-BoxStarterMessage 'apaga pacote temporario'
+$caminho = $env:ProgramData + '\chocolatey\lib\'
+Remove-Item –path $caminho -include *tmp* –recurse
+
+Write-BoxStarterMessage 'Configurando Shutup10'
+Invoke-WebRequest 'https://pastebin.com/raw/RUUZndqj' -outFile 'c:\layout\ooshutup10.cfg'
+oosu10.exe c:\layout\ooshutup10.cfg /quiet
+
+Write-BoxstarterMessage 'Configurando horario do windows'
+Invoke-WebRequest 'https://dl.dropbox.com/s/ke5empa6ecx8hv9/NetTime.exe' -outFile 'c:\layout\NetTime.exe'
+Invoke-WebRequest 'https://dl.dropbox.com/s/8nhtbtitju3k52o/NetTimeService.exe' -outFile 'c:\layout\NetTimeService.exe'
+Invoke-WebRequest 'https://pastebin.com/raw/PTt4Zi2G' -outFile 'c:\layout\atualizarhorario.bat'
+
+CriarRegistro('HKLM:\SOFTWARE\Subjective Software\NetTime')
+
+Set-ItemProperty -Path 'HKLM:\Software\Subjective Software\NetTime' -Name 'Hostname' -Type String -Value 'a.ntp.br'
+Set-ItemProperty -Path 'HKLM:\Software\Subjective Software\NetTime' -Name 'Port' -Type DWord -Value '123'
+Set-ItemProperty -Path 'HKLM:\Software\Subjective Software\NetTime' -Name 'Protocol' -Type DWord -Value '0'
+
+c:\layout\NetTime.exe /updateonce
+
+Write-BoxStarterMessage 'Criando tarefa do horario'
+$User= $env:UserName
+$Trigger= New-ScheduledTaskTrigger -AtLogon
+$Action= New-ScheduledTaskAction -Execute 'c:\layout\atualizarhorario.bat' -WorkingDirectory 'c:\layout\'
+Register-ScheduledTask -TaskName 'Atualizar Horario' -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
