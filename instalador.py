84ca8db66f602fc44334702312188cedf3b959eb
Murilo <murilobeluco@yahoo.com>
2021-08-02 20:06:41 -0300

    novo instalador

--- instalador.py
+++ instalador.py
@@ -1,95 +1,39 @@
 # Script para instalação do Chocolatey/Boxstarter
 
-import winreg, os, time
-from colorama import init, deinit, Fore
+import os
+import requests
 
-def executarComando(comando):
-	import subprocess
-	
-	print("Executando o comando: '{}'".format(comando), flush=True)
-	subprocess.call(comando, shell=True)
 
-def abrirLink(url=''):
-	import urllib.request
+def executar_comando(comando):
+    import subprocess
 
-	comando = urllib.request.urlopen(url)
-	
-	return comando
+    print("Executando o comando: '{}'".format(comando), flush=True)
+    subprocess.call(comando, shell=True)
 
-def reiniciarExplorer():
-	executarComando('taskkill /f /IM explorer.exe')
-	time.sleep(20)
-	executarComando('start explorer.exe')	
 
-def tratarComando(comando):
-	return comando.decode('utf-8').replace('\n', ' ').replace('\r', '')
+def criar_tempfile(sufixo, url, caminho=None, oculto=False):
+    import tempfile
 
-def mudarValorRegistro(keyval, escopo, nomeChave, tipo, valor):
-	try:
-		key = winreg.OpenKey(escopo, keyval, 0, winreg.KEY_ALL_ACCESS)	
-	except Exception:
-		key = winreg.CreateKey(escopo, keyval)
+    if caminho is not None:
+        if not os.path.exists(caminho):
+            os.mkdir(caminho)
+            if oculto:
+                os.popen('attrib +h ' + str(caminho))
 
-	winreg.SetValueEx(key, nomeChave, 0, tipo, valor)
+    temp_file, temp_path = tempfile.mkstemp(suffix=sufixo, dir=caminho, text=True)
 
-	winreg.CloseKey(key)
+    response = requests.get(url)
 
-def deletarIcones():
-	executarComando(r'del "C:\Users\Public\Desktop\Boxstarter Shell.lnk"')
-	executarComando(r'del "C:\Users\Public\Desktop\Microsoft Edge.lnk"')
-	executarComando(r'del "%USERPROFILE%\Desktop\Microsoft Edge.lnk"')
+    with open(temp_path, mode='w', encoding="utf-8") as f:
+        f.write(str(response.text))
 
-def criarTempFile(sufixo, url, caminho=None, oculto=False):
-	import tempfile
+    os.close(temp_file)
 
-	if caminho is not None:
-		if not os.path.exists(caminho):
-			os.mkdir(caminho)
-			if (oculto):
-				os.popen('attrib +h '+ str(caminho))	
+    return temp_path
 
-	tempFile, tempPath = tempfile.mkstemp(suffix=sufixo, dir=caminho, text=True)
 
-	comando = abrirLink(url)
-
-	with open(tempPath, mode='w', encoding="utf-8") as f:
-		for linhas in comando:
-			f.write(str(linhas.decode("utf-8")))
-	
-	os.close(tempFile)
-
-	return tempPath
-
-def removerApps():
-	caminho = criarTempFile('.ps1', 'https://pastebin.com/raw/xdjMmWi1', r'c:\layout', True)
-
-	executarComando('@powershell -NoProfile -ExecutionPolicy bypass -File ' + str(caminho))
-
-	os.remove(caminho)
-
-def removerOneDrive():
-	executarComando('taskkill /f /im OneDrive.exe')	
-	executarComando(r'%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall')
-	executarComando(r"rd %UserProfile%\OneDrive /Q /S")
-	executarComando(r"rd C:\OneDriveTemp /Q /S")
-	executarComando(r"REG Delete HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6} /f")
-	executarComando(r"REG Delete HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6} /f")
-
-def instalar():
-	executarComando('@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString(' + "'https://boxstarter.org/bootstrapper.ps1'" + ')); get-boxstarter -Force " ')
-	time.sleep(10)
-	executarComando('@powershell -NoProfile -ExecutionPolicy bypass -command "Import-Module ''C:/ProgramData/Boxstarter/Boxstarter.Chocolatey/Boxstarter.Chocolatey.psd1''; Invoke-ChocolateyBoxstarter -NoPassword -DisableRestart; Install-BoxstarterPackage -PackageName https://pastebin.com/raw/Eqp09mjj -DisableReboots"')
-
-	print(Fore.GREEN + '***configurando barra de tarefas***')
-	removerApps()
-	time.sleep(10)
-	deletarIcones()
-	time.sleep(10)
-	print(Fore.GREEN + '***Removendo OneDrive***')
-	removerOneDrive()
-	time.sleep(10)
-	
 if __name__ == "__main__":
-	init()
-	instalar()
-	deinit()
+    os.makedirs('C:\\layout', exist_ok=True)
+    tmp_file = criar_tempfile('.ps1', 'https://pastebin.com/raw/AGs7x79Q')
+    executar_comando(f'@powershell -NoProfile -ExecutionPolicy bypass -File {tmp_file}')
+    os.remove(tmp_file)
--- script.ps1
+++ script.ps1
@@ -1,127 +1,50 @@
-choco feature enable -n allowEmptyChecksums
-Update-ExecutionPolicy Unrestricted
-Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowHiddenFilesFoldersDrives -DisableShowFrequentFoldersInQuickAccess -enableShowRibbon
-Set-TaskbarOptions -Combine Never
-Disable-BingSearch
- 
-choco install Firefox googlechrome notepadplusplus.install shutup10 qbittorrent spotify discord.install fastcopy battle.net twitch authy-desktop megasync dropbox 7zip.install sharex  jre8 javaruntime freedownloadmanager steam razer-synapse-2 geforce-experience -y --ignore-checksums
- 
-Write-BoxstarterMessage 'Desativando modo de hibernacao'
-powercfg -h off
- 
-Write-BoxstarterMessage 'Mudando configuracao de energia: timeout do HD nunca'
-powercfg -change -disk-timeout-ac 0
-powercfg -change -disk-timeout-dc 0
- 
-Write-BoxstarterMessage 'Mudando configuracao de energia: Desligar monitor apos 20 minutos'
-powercfg -change -monitor-timeout-ac 10
-powercfg -change -monitor-timeout-dc 10
- 
-Write-BoxstarterMessage 'Mudando configuracao de energia: Standby do computador para nunca'
-powercfg -change -standby-timeout-ac 0
-powercfg -change -standby-timeout-dc 0
- 
-Set-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name 'InitialKeyboardIndicators' -Value 2
- 
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\7-Zip\", [EnvironmentVariableTarget]::Machine)
-refreshenv
- 
-New-Item -ItemType directory -Path 'C:\layout\'
- 
-Get-Item 'c:\layout' -Force | ForEach-Object { $_.Attributes = $_.Attributes -bor 'Hidden' }
- 
- 
-Write-BoxstarterMessage 'Configurando ShareX'
-Invoke-WebRequest 'https://dl.dropbox.com/s/8ns8lh850n5ppuh/ShareX-13.0.1-backup.sxb' -outFile 'c:\layout\sharexconfig.zip'
-                           
+Invoke-WebRequest 'https://dl.dropbox.com/s/mm1gyg1uha3vwae/ShareX-12.3.1-backup.sxb' -outFile 'c:\layout\sharexconfig.zip'
+
 $caminho = [environment]::getfolderpath("mydocuments") +'\' + 'Sharex'
-$comando = '& 7z.exe e c:\layout\sharexconfig.zip -o'+$caminho+' -y'
+$comando = '& "C:\Program Files\7-Zip\7z.exe" e c:\layout\sharexconfig.zip -o'+$caminho+' -y'
 Stop-Process -Name sharex | out-null
 Invoke-Expression $comando | out-null
+Start-Process -FilePath "C:\Program Files\ShareX\sharex.exe"
 
- 
+
 function CriarRegistro {
     Param ([string]$a)
-     
+
     if (-Not(Test-Path $a)) {
         New-Item -Path $a -Force | out-null
-    }    
+    }
 }
- 
-function CarregarExplorer {
-    Write-BoxStarterMessage 'Parando o processo Explorer.exe'
-    Stop-Process -Name Explorer | out-null
-    gpupdate /force
-    Start-Sleep 10
-}
- 
-Write-BoxStarterMessage 'Alterando configuracoes do registro'
-CriarRegistro('HKLM:\Software\Policies\Microsoft\Windows\Explorer')
-CriarRegistro('HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization')
-CriarRegistro('HKLM:\Software\Policies\Microsoft\Windows\OneDrive')
-CriarRegistro('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize')
-CriarRegistro('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu')
-CriarRegistro('HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer')
-CriarRegistro('HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People')
-CriarRegistro('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel')
- 
-#configurando menu iniciar
-Write-BoxStarterMessage 'Configurando menu iniciar'
-Invoke-WebRequest 'https://pastebin.com/raw/Rag0n8dQ' -outFile 'c:\layout\layout.xml'
-Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'LockedStartLayout' -Type DWord -Value 1
-Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'StartLayoutFile' -Type ExpandString -Value 'c:\layout\layout.xml'
-CarregarExplorer
-Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'LockedStartLayout' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKLM:Software\Policies\Microsoft\Windows\Explorer' -Name 'StartLayoutFile' -Type ExpandString -Value ''
-CarregarExplorer
- 
+
+
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization' -Name NoLockScreen -Type DWord -Value 1
-Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\Explorer' -Name 'HideRecentlyAddedApps' -Type DWord -Value 1
-Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSyncNGSC' -Type DWord -Value 1
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search' -Name 'SearchboxTaskbarMode' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize' -Name 'StartupDelayInMSec' -Type QWord -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People' -Name 'PeopleBand' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'ShowTaskViewButton' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Name 'SystemPaneSuggestionsEnabled' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Name 'SubscribedContent-338388Enabled' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'LaunchTo' -Type DWord -Value 1
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Start_TrackProgs' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel' -Name '{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu' -Name '{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarSmallIcons' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop' -Name 'IconSize' -Type DWord -Value 30
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop' -Name 'Mode' -Type DWord -Value 1
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop' -Name 'LogicalViewMode' -Type DWord -Value 3
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Start_TrackDocs' -Type DWord -Value 0
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer' -Name 'NoRecentDocsHistory' -Type DWord -Value 1
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarGlomLevel' -Type Dword -Value 2
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'DisableAntiSpyware' -Type Dword -Value 1
-Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Free Download Manager'
-Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Spotify'
-Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Discord'
- 
-Write-BoxStarterMessage 'apaga pacote temporario'
-$caminho = $env:ProgramData + '\chocolatey\lib\'
-Remove-Item –path $caminho -include *tmp* –recurse
+CriarRegistro('HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization')
 
-Write-BoxStarterMessage 'Configurando Shutup10'
-Invoke-WebRequest 'https://pastebin.com/raw/RUUZndqj' -outFile 'c:\layout\ooshutup10.cfg'
-oosu10.exe c:\layout\ooshutup10.cfg /quiet
 
-Write-BoxstarterMessage 'Configurando horario do windows'
 Invoke-WebRequest 'https://dl.dropbox.com/s/ke5empa6ecx8hv9/NetTime.exe' -outFile 'c:\layout\NetTime.exe'
 Invoke-WebRequest 'https://dl.dropbox.com/s/8nhtbtitju3k52o/NetTimeService.exe' -outFile 'c:\layout\NetTimeService.exe'
 Invoke-WebRequest 'https://pastebin.com/raw/PTt4Zi2G' -outFile 'c:\layout\atualizarhorario.bat'
+Start-Process 'c:\layout\atualizarhorario.bat'
 
 CriarRegistro('HKLM:\SOFTWARE\Subjective Software\NetTime')
+CriarRegistro('HKLM:\Software\WOW6432Node\Subjective Software\NetTime')
 
-Set-ItemProperty -Path 'HKLM:\Software\Subjective Software\NetTime' -Name 'Hostname' -Type String -Value 'a.ntp.br'
-Set-ItemProperty -Path 'HKLM:\Software\Subjective Software\NetTime' -Name 'Port' -Type DWord -Value '123'
-Set-ItemProperty -Path 'HKLM:\Software\Subjective Software\NetTime' -Name 'Protocol' -Type DWord -Value '0'
 
-c:\layout\NetTime.exe /updateonce
+powercfg -change -disk-timeout-ac 0
+powercfg -change -disk-timeout-dc 0
 
-Write-BoxStarterMessage 'Criando tarefa do horario'
+powercfg -change -monitor-timeout-ac 10
+powercfg -change -monitor-timeout-dc 10
+
+powercfg -change -standby-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+
+Set-ItemProperty -Path 'HKLM:\Software\WOW6432Node\Subjective Software\NetTime' -Name 'Hostname' -Type String -Value 'a.ntp.br'
+Set-ItemProperty -Path 'HKLM:\Software\WOW6432Node\Subjective Software\NetTime' -Name 'Port' -Type DWord -Value '123'
+Set-ItemProperty -Path 'HKLM:\Software\WOW6432Node\Subjective Software\NetTime' -Name 'Protocol' -Type DWord -Value '0'
+Set-ItemProperty -Path 'HKLM:\Software\WOW6432Node\Subjective Software\NetTime' -Name 'LogLevel' -Type DWord -Value '3'
+
+Start-Process 'c:\layout\atualizarhorario.bat'
+
 $User= $env:UserName
 $Trigger= New-ScheduledTaskTrigger -AtLogon
 $Action= New-ScheduledTaskAction -Execute 'c:\layout\atualizarhorario.bat' -WorkingDirectory 'c:\layout\'
